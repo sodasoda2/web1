dc869e3f0bc33dd2d6840ffa14da81e375ec2faf
sodasoda2 <qbru@naver.com>
2019-02-12 22:02:12 +0900

    Add files via upload

---
+++ test.php
@@ -0,0 +1,247 @@
+HTTP/1.1 200 OK
+Server: Hipcam
+Date: Sat, 09 Feb 2019 17:47:11 STD
+Last-Modified: Thu, 01 Jan 1970 09:10:12 STD
+Etag: "264.16c0"
+Content-Type: text/html
+Content-Length: 5824
+Connection: close
+Accept-Ranges: bytes
+
+ï»¿<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html><head>
+<meta content="text/html; charset=utf-8" http-equiv="content-type">
+<script src="cgi-bin/hi3510/param.cgi?cmd=getuserinfo" type="text/javascript"></script>
+<script type="text/javascript" src="js/jquery.js"></script>
+<link href="kang.css" rel="stylesheet" type="text/css">
+<script type="text/javascript" src="public.js"></script>
+<script type="text/javascript">
+var language0;
+function l1()
+{
+setcookie('language0',"1",720);
+window.location.reload();
+}
+
+function l0()
+{
+setcookie('language0',"0",720);
+window.location.reload();
+}
+
+function l2()
+{
+setcookie('language0',"2",720);
+window.location.reload();
+}
+function l3()
+{
+setcookie('language0',"3",720);
+window.location.reload();
+}
+
+function l4()
+{
+setcookie('language0',"4",720);
+window.location.reload();
+}
+
+function l5()
+{
+setcookie('language0',"5",720);
+window.location.reload();
+}
+
+function l6()
+{
+setcookie('language0',"6",720);
+window.location.reload();
+}
+
+function l7()
+{
+setcookie('language0',"7",720);
+window.location.reload();
+}
+function l8()
+{
+setcookie('language0',"8",720);
+window.location.reload();
+}
+function l9()
+{
+setcookie('language0',"9",720);
+window.location.reload();
+}
+function l10()
+{
+setcookie('language0',"10",720);
+window.location.reload();
+}
+function l11()
+{
+setcookie('language0',"11",720);
+window.location.reload();
+}
+function l12()
+{
+setcookie('language0',"12",720);
+window.location.reload();
+}
+
+function playback()
+{
+if(authLevel==15)
+window.open('backplay.html','playback','height=600,width=1000,top=100,left=100,toolbar=no,menubar=no,scrollbars=yes,location=no,status=no');
+else if(authLevel==3)
+window.open('backplay_u.html','playback','height=600,width=1000,top=100,left=100,toolbar=no,menubar=no,scrollbars=yes,location=no,status=no');
+else if(authLevel==1)
+window.open('backplay_g.html','playback','height=600,width=1000,top=100,left=100,toolbar=no,menubar=no,scrollbars=yes,location=no,status=no');
+
+}
+</script>
+<title></title>
+<style type="text/css">
+<!-- 
+body{margin-top:0px;
+     margin-left:0px;
+	 margin-right:0px;
+     background-color: #E8E8E8;} 
+a{TEXT-DECORATION:none}
+a:hover{TEXT-DECORATION:none}
+
+.STYLE1 {
+	color: #FFFFFF;
+	font-size: 13px;
+	font-weight: bold;
+}
+.STYLE2 {color: #F08200;
+    font-size: 13px;
+    font-weight: bold;}
+.STYLE3 {
+    font-weight: bold;}
+.STYLE4 {color: #3827FE;
+    font-weight: bold;}
+-->
+</style>
+</head>
+
+<body>
+<div><table width="100%"  height="25" border="0" cellpadding="0" cellspacing="0" bgcolor="#00599D">
+  <tr>
+    <td width="25%" align="right"><span class="STYLE1">
+	<script type="text/javascript">document.write(str_language);</script>:</span>&nbsp;&nbsp;&nbsp;</td>
+    <td>
+	<a href="javascript:l0();" >
+		<span class="STYLE1">English</span></a>
+	&nbsp;&nbsp;<span class="STYLE1">|</span>&nbsp;&nbsp;
+		<a href="javascript:l1();" >
+		<span class="STYLE1">
+		ç®ä½ä¸­æ</span></a>
+	&nbsp;&nbsp;
+	
+		<a href="javascript:l2();" >
+		<span class="STYLE1">
+		FranÃ§ais</span></a>
+	&nbsp;&nbsp;
+	
+	<a href="javascript:l3();" >
+		<span class="STYLE1">
+		Swedish</span></a>
+	&nbsp;&nbsp;
+		
+		<a href="javascript:l4();" >
+		<span class="STYLE1">
+		EspaÃ±ol</span></a>
+	&nbsp;&nbsp;
+	
+		<a href="javascript:l5();" >
+		<span class="STYLE1">
+		PortuguÃªs</span></a>
+	&nbsp;&nbsp;
+	
+		<a href="javascript:l6();" >
+		<span class="STYLE1">
+		Italiano</span></a>
+	&nbsp;&nbsp;
+	
+		<a href="javascript:l7();" >
+		<span class="STYLE1">
+		Deutsch</span></a>
+	&nbsp;&nbsp;
+	
+	<a href="javascript:l8();" >
+		<span class="STYLE1">
+		PÑÑÑÐºÐ¸Ð¹</span></a>
+	&nbsp;&nbsp;
+	
+	<a href="javascript:l9();" >
+		<span class="STYLE1">
+		íêµ­ì</span></a>
+	&nbsp;&nbsp;
+	
+	<a href="javascript:l10();" >
+		<span class="STYLE1">
+		Nederlands</span></a>
+	&nbsp;&nbsp;
+	
+	<a href="javascript:l11();" >
+		<span class="STYLE1">
+		æ¥æ¬èª</span></a>
+	&nbsp;&nbsp;
+    <a href="javascript:l12();" >
+		<span class="STYLE1">
+		Polish</span></a>
+	&nbsp;&nbsp;
+     
+	  
+	&nbsp;&nbsp;<span class="STYLE1">|</span>&nbsp;&nbsp;
+	<a href="/web/ClientPlus_Setup.exe"> <span class="STYLE2"><img src="images/ocx.png"  style="border:0"  /></span>
+      </a></td>
+    </tr>
+</table>
+</div>
+<br><br><br>
+<div>
+<table width="58%" align="center" border="0" cellspacing="0" cellpadding="0">
+
+  <tr>
+    <td><img src="images/firefox.png"></td>
+    <td><span class="STYLE3"><script type="text/javascript">document.write(str_IE2);</script></span></td>
+  </tr>
+  <tr>
+    <td height="30" >&nbsp;</td>
+    <td ><a class="first_a" href="/web/index.html"><script type="text/javascript">document.write(str_login);</script></a></td>
+  </tr>
+  <tr>
+    <td height="20" colspan="2" ><img src="images/line.jpg"></td>
+    </tr>
+  <tr>
+    <td><img src="images/iPhone.gif"></td>
+    <td><span class="STYLE3"><script type="text/javascript">document.write(str_IE3);</script></span></td>
+  </tr>
+  <tr>
+    <td height="30" >&nbsp;</td>
+    <td ><a class="first_a" href="/web/mobile.html"><script type="text/javascript">document.write(str_login);</script></a></td>
+  </tr>
+  <tr>
+    <td height="20" colspan="2" ><img src="images/line.jpg"></td>
+    </tr>
+  <tr>
+    <td><img src="images/luxiang.png"></td>
+    <td><span class="STYLE3"><script type="text/javascript">document.write(str_IE4);</script></span></td>
+  </tr>
+  <tr>
+    <td height="30" >&nbsp;</td>
+    <td ><a class="first_a" href="###" onClick="playback()"><script type="text/javascript">document.write(str_login);</script></a></td>
+  </tr>
+  <tr>
+    <td height="20" colspan="2" ><img src="images/line.jpg"></td>
+    </tr>
+</table>
+
+</div>
+
+	
+</body>
+</html>
